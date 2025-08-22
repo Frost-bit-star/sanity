d0f9be134adffa7905fcaadfed904384e6f9bf67
Frost-bit-star <morganmilstone983@gmail.com>
2025-08-22 12:33:20 +0300

    first commit

---
+++ studio/schemas/homepage.js
@@ -0,0 +1,9 @@
+export default {
+  name: "homepage",
+  title: "Homepage",
+  type: "document",
+  fields: [
+    { name: "title", type: "string", title: "Title" },
+    { name: "description", type: "text", title: "Description" }
+  ]
+};
